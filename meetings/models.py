a017073f0e59f5b3cea8395731096133bfb89599
Jake Prem <jakeprem@gmail.com>
2016-12-13 21:12:37 -0500

    Initial commit. Currently downloads and populates models.
    The app can currently download session and speaker data and split
    them out into various tables with relations.

---
+++ meetings/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+
+# Create your models here.
+class Speaker(models.Model):
+    id = models.UUIDField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=200)
+
+    gravatar_url = models.URLField(null=True)
+    biography = models.TextField()
+    blog_link = models.URLField(null=True)
+
+    github_link = models.URLField(null=True)
+    twitter_link = models.URLField(null=True)
+    linkedin_link = models.URLField(null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+
+class Tag(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class Room(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class Category(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class SessionType(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class Meeting(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200, null=True)
+    abstract = models.TextField(null=True)
+
+    speakers = models.ManyToManyField(Speaker)
+    tags = models.ManyToManyField(Tag)
+
+    session_type = models.ForeignKey(SessionType, null=True)
+    session_time = models.CharField(max_length=100, null=True)
+
+    category = models.ForeignKey(Category, null=True)
+    room = models.CharField(max_length=200, null=True)
+    rooms = models.ManyToManyField(Room)
+
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.title
+
---
+++ meetings/utils.py
@@ -0,0 +1,73 @@
+from urllib.request import urlopen
+import json
+
+from django.core.exceptions import ObjectDoesNotExist
+
+from meetings.models import Meeting, Speaker, Tag, SessionType, Room, Category
+
+
+def download_meeting_data():
+    url = 'https://speakers.codemash.org/api/SessionsData?type=json'
+    response = urlopen(url)
+    data = json.loads(response.read().decode('utf-8'))
+
+    for meeting in data:
+        _ingest_meeting(meeting)
+
+
+def _ingest_meeting(meeting):
+    new_meeting = Meeting()
+    new_meeting.id = meeting['Id']
+    new_meeting.title = meeting['Title']
+    new_meeting.abstract = meeting['Abstract']
+    new_meeting.session_time = meeting['SessionTime']
+    new_meeting.save()
+    new_meeting.room = meeting['Room']
+    new_meeting.start_time = meeting['SessionStartTime']
+    new_meeting.end_time = meeting['SessionEndTime']
+
+    for speaker in meeting['Speakers']:
+        new_meeting.speakers.add(speaker['Id'])
+
+    for tag in meeting['Tags']:
+        tag_model, created = Tag.objects.get_or_create(text=tag)
+        new_meeting.tags.add(tag_model)
+
+    session_type_model, created = SessionType.objects.get_or_create(text=meeting['SessionType'])
+    new_meeting.session_type = session_type_model
+
+    category_model, created = Category.objects.get_or_create(text=meeting['Category'])
+    new_meeting.category = category_model
+
+    for room in meeting['Rooms']:
+        room_model, created = Room.objects.get_or_create(text=room)
+        new_meeting.rooms.add(room_model)
+
+    new_meeting.save()
+
+
+def download_speaker_data():
+    url = 'https://speakers.codemash.org/api/SpeakersData?type=json'
+    response = urlopen(url)
+    data = json.loads(response.read().decode('utf-8'))
+
+    for speaker in data:
+        _ingest_speaker(speaker)
+
+
+def _ingest_speaker(speaker):
+    new_speaker = Speaker()
+
+    new_speaker.id = speaker['Id']
+    new_speaker.first_name = speaker['FirstName']
+    new_speaker.last_name = speaker['LastName']
+
+    new_speaker.gravatar_url = speaker['GravatarUrl']
+    new_speaker.biography = speaker['Biography']
+    new_speaker.blog_link = speaker['BlogUrl']
+
+    new_speaker.github_link = speaker['GitHubLink']
+    new_speaker.twitter_link = speaker['TwitterLink']
+    new_speaker.linkedin_link = speaker['LinkedInProfile']
+
+    new_speaker.save()
