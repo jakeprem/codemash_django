10e31eb895a4f8f53ef0665aac0696376bab2546
Jake Prem <jakeprem@gmail.com>
2016-12-14 20:16:53 -0500

    Added speaker functionality to the admin site
    Added more complete functionality to speakers and made speakers
    and meetings read only.

--- meetings/admin.py
+++ meetings/admin.py
@@ -7,7 +7,8 @@ class MeetingAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'tags_list', 'date', 'start_time', 'end_time', 'speaker_names']
     list_select_related = ('category',)
     search_fields = ['title', 'date', 'start_time', 'end_time', 'speakers__first_name', 'speakers__last_name', 'tags__text']
-
+    readonly_fields = ['id', 'title', 'abstract', 'speakers', 'tags', 'session_type', 'session_time', 'category',
+                       'room', 'rooms', 'start_datetime', 'end_datetime', 'start_time', 'end_time', 'date']
 
     def speaker_names(self, obj):
         speakers_list = []
@@ -24,6 +25,9 @@ class MeetingAdmin(admin.ModelAdmin):
 
 class SpeakerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'blog_link', 'github_link', 'id']
+    search_fields = ['first_name', 'last_name']
+    readonly_fields = ['id', 'first_name', 'last_name', 'gravatar_url', 'biography', 'blog_link',
+                       'github_link', 'twitter_link', 'linkedin_link']
 
 admin.site.register(Meeting, MeetingAdmin)
 admin.site.register(Speaker, SpeakerAdmin)
